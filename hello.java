bd5a7b1ba2ab361316dd2cfccb02143108e999ec
shivam-kumar-singh <boltshivam001@gmail.com>
2018-09-11 06:34:16 +0000

    add

--- hello.java
+++ hello.java
@@ -1,11 +1,7 @@
 public class hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-<<<<<<< HEAD
-      System.out.println("Hello, World, SHIVAM");
-=======
       System.out.println("Hello,World");
-      
->>>>>>> b17ba669045ebe03a22283f092092f9a702cc65e
+     
    }
 }
