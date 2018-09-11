0bc45882eec629d5125ad4caed030918ba13afe9
shivam-kumar-singh <40204468+shivam-kumar-singh@users.noreply.github.com>
2018-09-11 10:50:24 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World made by shivam");
+      System.out.println("Hello, World made by shivam");
    }
 }
